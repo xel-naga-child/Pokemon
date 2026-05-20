85d3348a75b08d7ddb86decc86d20ea86b2f6ddc
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-05-19 22:32:25 -0400

    Add Unique entity

---
+++ Concept/PokeConceptBL/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿namespace PokeConceptBL;
+
+public class Pokemon
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Base_Experience { get; set; }
+    public int Height { get; set; }
+    public int is_default { get; set; }
+    public int Order { get; set; }
+}
