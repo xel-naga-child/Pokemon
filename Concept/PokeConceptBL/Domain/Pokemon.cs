69dfe907d9c8fdd19bdb7619b91037a935c6ed02
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-05-23 00:57:44 -0400

    Nhibernate cfg

--- Concept/PokeConceptBL/Domain/Pokemon.cs
+++ Concept/PokeConceptBL/Domain/Pokemon.cs
@@ -2,8 +2,9 @@
 
 public class Pokemon
 {
+    public Guid guid {get; set;}
     public int Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public int Base_Experience { get; set; }
     public int Height { get; set; }
     public int is_default { get; set; }
