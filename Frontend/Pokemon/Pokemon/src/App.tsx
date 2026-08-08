b0ecebfbb6d8353ac9b4fd806b69d11634ccb795
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-08-08 01:59:15 -0400

    PokeService

---
+++ Frontend/Pokemon/Pokemon/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+        </div>
+
+        <div>
+        </div>
+        
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+       
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/Pokemon/Pokemon/src/test/serviceTest.test.ts
@@ -0,0 +1,8 @@
+import {expect, test} from 'vitest';
+import {PokemonListService} from '../services/PokeServices';
+
+test('PokemonListServiceTest', async () => {   
+
+    expect(await PokemonListService(0, 10)).toBeDefined();
+
+ })
