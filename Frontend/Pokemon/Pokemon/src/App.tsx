b0265f1374e6a5367e921e11700cedcfbb2fd094
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-08-14 23:38:40 -0400

    add style

--- Frontend/Pokemon/Pokemon/src/App.tsx
+++ Frontend/Pokemon/Pokemon/src/App.tsx
@@ -1,26 +1,24 @@
-import './App.css'
-import {PokemonList} from './presentation/Pages/PokemonList/PokemonList.uc.tsx'
-import { Menu } from 'antd';
+import "./App.css";
+import { PokemonList } from "./presentation/Pages/PokemonList/PokemonList.uc.tsx";
+import { Menu } from "antd";
 
 function App() {
-
   let items = [
     {
-      label: 'Pokemon',
-      key: 'pokemon',
+      label: "Pokemon",
+      key: "pokemon",
     },
     {
-      label: 'Pokemon2',
-      key: 'pokemon2',
+      label: "Pokemon2",
+      key: "pokemon2",
     },
   ];
   return (
     <>
-    <Menu items={items} mode='vertical' />
-    <PokemonList />
-
+      <Menu items={items} mode="vertical" className="menu" />
+      <PokemonList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
